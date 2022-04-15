c00f86d5fc29dad2886ca52adcd35818d82ae59b
lilit_ad <lilith.adoyan@gmail.com>
2022-04-15 14:20:56 +0400

    Register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Layout from '../components/Layout'
 function RegisterPage() {
-  return (
-    <Layout><h1>RegisterPage</h1></Layout>
-  )
+  return (<Layout>
+    <div className='register-parent'>
+      <div className='row'>
+        <div className='col-md-5'>
+
+<lottie-player src="https://assets10.lottiefiles.com/packages/lf20_yr6zz3wv.json"  background="transparent"  speed="1" autoplay></lottie-player>
+        </div>
+        <div className='col-md-5'>
+          <div className='login-form'>
+<form >
+<h2>Register now</h2>
+<hr/>
+<input/>
+</form>
+</div>
+        </div>
+      </div>
+    </div></Layout>
+    )
 }
 
-export default RegisterPage
+export default RegisterPage;
